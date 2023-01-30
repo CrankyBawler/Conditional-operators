6757973c3db8c1b1dbb4b9b84a3c56f474f90c11
Cranky Bawler <6666lenia6666@mail.ru>
2023-01-30 19:35:19 +0300

    Задание №5

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
         task1();
         task2();
         task3();
-
+        task4();
+        task5();
+        task6();
     }
-
     public static void task1() {
         System.out.println("Задача 1");
         int age = 25;
@@ -33,4 +34,47 @@ public class Main {
             System.out.println("Если скорость " + speed + " можно ездить спокойно");
         }
         }
-}
+    public static void task4() {
+        System.out.println("");
+        System.out.println("Задача 4");
+        int age = 17;
+        boolean goToKindergarten = age > 2 && age < 6;
+        if (goToKindergarten) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в детский сад");
+        }
+        boolean goToSchool = age > 7 && age < 18;
+        if (goToSchool) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в школу");
+        }
+        boolean goToUniversity = age > 18 && age < 24;
+        if (goToUniversity) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить в университет");
+        }
+        boolean goToWork = age > 24;
+        if (goToWork) {
+            System.out.println("Если возраст человека равен " + age + " то ему нужно ходить на работу");
+        }
+    }
+    public static void task5() {
+        System.out.println("");
+        System.out.println("Задача 5");
+        int age = 15;
+        boolean youCantRide  = age < 5;
+        if (youCantRide) {
+            System.out.println("Если возраст ребенка равен " + age + " то ему нельзя кататься на аттракционе");
+        }
+        boolean onlyWithAnAdult = age > 5 && age < 14;
+        if (onlyWithAnAdult) {
+            System.out.println("Если возраст ребенка равен " + age + " то он может кататься только в сопровождении взрослого." +
+                    "Если взрослого нет, то кататься нельзя");
+        }
+        boolean youCanRide = age > 14;
+        if (youCanRide){
+            System.out.println("Если возраст ребенка равен " + age + " то он может кататься без сопровождения взрослого.");
+        }
+        }
+    public static void task6() {
+        System.out.println("");
+        System.out.println("Задача 6");
+
+    }
