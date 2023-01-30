69940b5d95b1adf0c2595884269a3d62b1f4d9f5
Cranky Bawler <6666lenia6666@mail.ru>
2023-01-30 18:26:01 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+            }
+            public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 15;
+        if (age >= 18){
+            System.out.printf("Если возраст человека равен " + age + " то он совершеннолетний");
+        }else {
+            System.out.printf("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного " +
+                    "подождать");}
+
     }
-}
+    }
